05c49a42cc10bf663d05e738ee1dbe2f9916fdcf
oro7l <xz305@naver.com>
2023-04-03 02:42:41 +0900

    week4_assingment

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
-
+from django.views.decorators.http import require_http_methods
+from .models import Post
 # Create your views here.
 
 def hello_world(request):
@@ -34,3 +35,33 @@ def code_reviewer_info(request):
             }
             ]
         })
+    
+@require_http_methods(["GET"]) 
+def get_post_detail(request, id):
+    post = get_object_or_404(Post, pk = id)
+    category_json={
+        "id"    : post.post_id,
+        "writer": post.writer,
+        "content": post.content,
+        "category" : post.category,
+    }
+    return JsonResponse({
+        'status' : 200,
+        'message' : '게시글 조회 성공',
+        'data' : category_json
+    })
+
+@require_http_methods(["GET"]) 
+def get_post_all(request):
+    post = Post.objects.all()
+    category_json={
+        "id"    : post.post_id,
+        "writer": post.writer,
+        "content": post.content,
+        "category" : post.category,
+    }
+    return JsonResponse({
+        'status' : 200,
+        'message' : '게시글 조회 성공',
+        'data' : category_json
+    })
