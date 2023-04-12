bd87f4adac5b572dc16cfa7cea2185e78976fb54
Na Yeongkyeong <102007066+na-yk@users.noreply.github.com>
2023-04-12 20:28:21 +0900

    fix: Add helloworld url in posts url

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('<int:id>/', post_detail, name="post_detail"),
     path('new/', create_post, name="create_post"),
     path('', get_post_all, name="get_post_all"),
-    path('comment/<int:post_id>/', get_comment, name='get_comment')
+    path('comment/<int:post_id>/', get_comment, name='get_comment'),
+    path('',hello_world, name="hello_world")
 ]
